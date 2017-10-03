38eca38b3e55201a0ede463ec0a0b49dd9ea7e20
Fabian Gut <fabian.gut@ethy.ch>
2017-10-03 06:37:25 +0200

    Unit tests for ApiController

---
+++ tests/AppBundle/Controller/ApiControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApiControllerTest extends WebTestCase
+{
+    public function testRedirectionForGet()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/api');
+        $response = $client->getResponse();
+
+        $this->assertEquals(301, $response->getStatusCode());
+        $this->assertEquals('http://localhost/', $response->headers->get('location'));
+    }
+
+    public function testPostWithNoQuery()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', '/api');
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(
+            '{"data":{"hello":"Your GraphQL endpoint is ready! Please log in to see the full API."}}',
+            $response->getContent()
+        );
+    }
+
+    public function testLogin()
+    {
+        $query = '{"query":"mutation{\n  createToken(username: \"fgu\", password: \"test\"){\n    error\n    token\n  }\n}","variables":null}';
+        $client = $this->sendApiQuery($query);
+        $response = $client->getResponse();
+        $content = $response->getContent();
+        $json = json_decode($content);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertNotNull($json->data->createToken->token);
+        $this->assertFalse(property_exists($json->data->createToken, 'error'));
+    }
+
+    public function testLoginWithWrongPassword()
+    {
+        $query = '{"query":"mutation{\n  createToken(username: \"fgu\", password: \"test123\"){\n    error\n    token\n  }\n}","variables":null}';
+        $client = $this->sendApiQuery($query);
+        $response = $client->getResponse();
+        $content = $response->getContent();
+        $json = json_decode($content);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertFalse(property_exists($json->data->createToken, 'token'));
+        $this->assertEquals('Invalid username or password', $json->data->createToken->error);
+    }
+
+    public function testLoginWithInvalidUser()
+    {
+        $query = '{"query":"mutation{\n  createToken(username: \"invalid\", password: \"test\"){\n    error\n    token\n  }\n}","variables":null}';
+        $client = $this->sendApiQuery($query);
+        $response = $client->getResponse();
+        $content = $response->getContent();
+        $json = json_decode($content);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertFalse(property_exists($json->data->createToken, 'token'));
+        $this->assertEquals('Invalid username or password', $json->data->createToken->error);
+    }
+
+    private function sendApiQuery($query) {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            $query);
+        return $client;
+    }
+}
