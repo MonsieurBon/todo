87f97ed562226fbc12fb3cec2a2e981727b30ca8
Fabian Gut <fabian.gut@ethy.ch>
2017-11-04 14:10:07 +0100

    Print CORS headers if requested

---
+++ src/AppBundle/EventSubscriber/CorsListener.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class CorsListener implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST => array('onKernelRequest', 0),
+            KernelEvents::RESPONSE => array('onKernelResponse', 0)
+        );
+    }
+
+    public function onKernelRequest(GetResponseEvent $event) {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        $method = $request->getRealMethod();
+
+        if (Request::METHOD_OPTIONS === $method) {
+            $response = new Response();
+            $event->setResponse($response);
+        }
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event) {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:4200');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST');
+        $response->headers->set('Access-Control-Allow-Headers', 'CONTENT-TYPE, X-AUTH-TOKEN');
+    }
+}
